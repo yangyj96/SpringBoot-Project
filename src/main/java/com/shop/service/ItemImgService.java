9b45fe636947e9e575eb62f50a5c33177de4370a
yangyj96 <yangyj96@naver.com>
2023-04-03 18:55:27 +0900

    img 업로드 경로 수정_5

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -38,7 +38,7 @@ public class ItemImgService {
         //파일 업로드
         if(!StringUtils.isEmpty(oriImgName)) {
             imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
-            imgUrl = "/images/" + imgName;
+            imgUrl = "/images/item/" + imgName;
         }
 
         //상품 이미지 정보 저장
@@ -58,7 +58,7 @@ public class ItemImgService {
 
             String oriImgName = itemImgFile.getOriginalFilename();
             String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
-            String imgUrl = "/images/" + imgName;
+            String imgUrl = "/images/item/" + imgName;
             savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
         }
     }
